5f76777c67878acc922bd7520539bfc004f2fd4e
rheeze-dev <ggybzz2297@gmail.com>
2023-06-16 12:39:50 -0400

    Completed logic for main country and neighbor

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
-async function getResponse(country, neighbors = 0) {
+async function getResponse(country, numberOfNeighbors = 0) {
 	const response = await fetch(
-        "https://countries-api-836d.onrender.com/countries/name/" + country,
-		{
-			method: 'GET',
-			headers: {
-				'X-Api-Key': '54/p8rt+p9QhgeN9G/Z5Sg==wrJ1tX7OT2EAdJcR',
-                "Content-type": "application/json; charset=UTF-8"
-			}
-		}
+        "https://countries-api-836d.onrender.com/countries/name/" + country
 	);
 	if (!response.ok) {
 		throw new Error(`HTTP error! status: ${response.status}`);
@@ -15,13 +8,16 @@ async function getResponse(country, neighbors = 0) {
 	const data = await response.json();
     const neighborsArr = data[0].borders;
     console.log(neighborsArr);
-    displayCountries(data, neighbors, neighborsArr)
+    displayCountries(data, numberOfNeighbors, neighborsArr)
 }
 
-async function displayCountries(country, neighbors, bordersArr) {
+async function displayCountries(country, numberOfNeighbors, bordersArr) {
     document.querySelector("#main-country").insertAdjacentHTML("beforeend", 
     `
-    <img class="country-img" src="${country[0].flag}" />
+    <div>
+        <img class="country-img" src="${country[0].flag}" />
+        <h3 class="country-name">Total number of borders: ${country[0].borders.length}</h3>
+    </div>
     <article class="country">
     <div class="country-data">
         <h3 class="country-name">${country[0].name}</h3>
@@ -31,31 +27,31 @@ async function displayCountries(country, neighbors, bordersArr) {
         <p class="country-row"><span>🗣️</span>${country[0].languages[0].name}</p>
         <p class="country-row"><span>💰</span>${country[0].currencies[0].name}</p>
     </div>
-    </article>`);
-    if(neighbors >= bordersArr.length) neighbors = bordersArr.length;
-    for(let i = 0; i < neighbors; i++) {
-    const res = await fetch(`https://countries-api-836d.onrender.com/countries/alpha/${bordersArr[i]}`);
-    if (!res.ok) {
-		throw new Error(`HTTP error! status: ${res.status}`);
-	}
-    const data = await res.json();
-    // console.log(data);
-    const neighbour = data;
-    
-    document.querySelector(`.neighbor${i+1}`).insertAdjacentHTML("beforeend", 
-        `
-        <img class="neighbor-img" src="${neighbour.flag}" />
-        <article class="neighbor">
-            <div class="neighbor-data">
-            <h3 class="neighbor-name">${neighbour.name}</h3>
-            <h4 class="neighbor-region">${neighbour.region}</h4>
-            <p class="neighbor-row"><span>🏙</span> ${neighbour.capital}</p>
-            <p class="neighbor-row"><span>👫</span>${neighbour.population}</p>
-            <p class="neighbor-row"><span>🗣️</span>${neighbour.languages[0].name}</p>
-            <p class="neighbor-row"><span>💰</span>${neighbour.currencies[0].name}</p>
-            </div>
-        </article>`);
+    </article>
+    `);
+    if(numberOfNeighbors >= bordersArr.length) numberOfNeighbors = bordersArr.length;
+    for(let i = 0; i < numberOfNeighbors; i++) {
+        const res = await fetch(`https://countries-api-836d.onrender.com/countries/alpha/${bordersArr[i]}`);
+        if (!res.ok) {
+            throw new Error(`HTTP error! status: ${res.status}`);
+        }
+        const neighbor = await res.json();
+        
+        document.querySelector(`.neighbor${i+1}`).insertAdjacentHTML("beforeend", 
+            `
+            <img class="neighbor-img" src="${neighbor.flag}" />
+            <article class="neighbor">
+                <div class="neighbor-data">
+                <h3 class="neighbor-name">${neighbor.name}</h3>
+                <h4 class="neighbor-region">${neighbor.region}</h4>
+                <p class="neighbor-row"><span>🏙</span> ${neighbor.capital}</p>
+                <p class="neighbor-row"><span>👫</span>${neighbor.population}</p>
+                <p class="neighbor-row"><span>🗣️</span>${neighbor.languages[0].name}</p>
+                <p class="neighbor-row"><span>💰</span>${neighbor.currencies[0].name}</p>
+                </div>
+            </article>
+            `);
     }
 }
 
-getResponse("usa", 2);
+getResponse("laos", 3);
