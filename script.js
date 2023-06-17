858f3ef09a520584f379b13a5d6740375fde1bdb
rheeze-dev <ggybzz2297@gmail.com>
2023-06-17 17:27:59 -0400

    Added land area of the countries in the API call

--- script.js
+++ script.js
@@ -15,13 +15,15 @@ async function getCountryApi(country, numberOfNeighbors = 0) {
 	const response = await fetch(
         "https://countries-api-836d.onrender.com/countries/name/" + country
 	);
-	if (!response.ok) {
+	if(!response.ok) {
         document.querySelector("#main-country").innerHTML = 
             `<div class="error-message">The country you entered does not exist, please enter a valid country!</div>`;
 		throw new Error(`HTTP error! status: ${response.status}`);
 	}
 	const data = await response.json();
     const neighborsArr = data[0].borders;
+    console.log(data[0]);
+    console.log(data[0].area);
     displayCountries(data, numberOfNeighbors, neighborsArr)
 }
 
@@ -29,8 +31,9 @@ async function displayCountries(country, numberOfNeighbors, bordersArr) {
     document.querySelector("#main-country").innerHTML =
     `
     <div>
-        <img class="country-img" src="${country[0].flag}" />
-        <h3 class="country-name">Total number of borders: ${country[0].borders.length}</h3>
+        <div><img class="country-img" src="${country[0].flags.png}" /></div>
+        <div class="total-borders-text">Total number of borders:</div>
+        <div class="total-borders">${country[0].borders.length}</div>
     </div>
     <article class="country">
     <div class="country-data">
@@ -38,6 +41,7 @@ async function displayCountries(country, numberOfNeighbors, bordersArr) {
         <h4 class="country-region">${country[0].region}</h4>
         <p class="country-row"><span>🏙</span> ${country[0].capital}</p>
         <p class="country-row"><span>👫</span>${country[0].population.toLocaleString()}</p>
+        <p class="country-row"><span>🌎</span>${country[0].area.toLocaleString()} sq km</p>
         <p class="country-row"><span>🗣️</span>${country[0].languages[0].name}</p>
         <p class="country-row"><span>💰</span>${country[0].currencies[0].name}</p>
     </div>
@@ -53,13 +57,14 @@ async function displayCountries(country, numberOfNeighbors, bordersArr) {
         
         document.querySelector(`.neighbor${i+1}`).innerHTML =  
             `
-            <img class="neighbor-img" src="${neighbor.flag}" />
+            <img class="neighbor-img" src="${neighbor.flags.png}" />
             <article class="neighbor">
                 <div class="neighbor-data">
                 <h3 class="neighbor-name">${neighbor.name}</h3>
                 <h4 class="neighbor-region">${neighbor.region}</h4>
                 <p class="neighbor-row"><span>🏙</span> ${neighbor.capital}</p>
                 <p class="neighbor-row"><span>👫</span>${neighbor.population.toLocaleString()}</p>
+                <p class="country-row"><span>🌎</span>${country[0].area.toLocaleString()} sq km</p>
                 <p class="neighbor-row"><span>🗣️</span>${neighbor.languages[0].name}</p>
                 <p class="neighbor-row"><span>💰</span>${neighbor.currencies[0].name}</p>
                 </div>
